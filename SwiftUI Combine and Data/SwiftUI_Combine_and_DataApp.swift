1d2f899898ce452b3983b4debb02be9ed0332c0d
Nikita Kosolapov <kosolapovnikitav@gmail.com>
2024-03-13 15:58:46 +0300

    First commit

---
+++ SwiftUI Combine and Data/SwiftUI_Combine_and_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_and_DataApp.swift
+//  SwiftUI Combine and Data
+//
+//  Created by Nikita Kosolapov on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_and_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
